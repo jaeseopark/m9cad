df858c6132aff82fc46d8374ae75487e69f849d5
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-09-15 08:43:28 -0600

    feat: apple quicktime video format

--- src/util/mimeUtils.ts
+++ src/util/mimeUtils.ts
@@ -2,5 +2,6 @@
 
 export const guessMimetypeAsync = async (filepath: string) => {
   if (filepath.endsWith(".mp4")) return "video/mp4";
+  else if (filepath.endsWith(".mov")) return "video/quicktime";
   throw new Error("No mimetype detected");
 };
